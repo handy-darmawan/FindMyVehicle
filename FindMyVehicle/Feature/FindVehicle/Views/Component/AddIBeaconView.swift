77ffcb80334f5ba0aae3c78a780b6de3cde9f65a
ndyy <handy.darmawan@binus.ac.id>
2023-05-23 22:06:42 +0700

    Initial Commit - bug when viewing data

---
+++ FindMyVehicle/Feature/FindVehicle/Views/Component/AddIBeaconView.swift
@@ -0,0 +1,53 @@
+//
+//  AddIBeaconView.swift
+//  FindVehicle
+//
+//  Created by ndyyy on 23/05/23.
+//
+
+import SwiftUI
+
+struct AddIBeaconView: View {
+    @ObservedObject var homeVM: HomeViewModel
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextFieldComponentView(title: "Identifier Name", placeholder: "ex Honda Motorcycle", answer: $homeVM.iBeaconVM.name)
+                    .padding(.bottom, 15)
+                TextFieldComponentView(title: "IBeacon UUID", placeholder: "ex XXXX-XXX", answer: $homeVM.iBeaconVM.uuid)
+                    .padding(.bottom, 15)
+                TextFieldComponentView(title: "IBeacon Major", placeholder: "ex 12", answer: $homeVM.iBeaconVM.major)
+                    .padding(.bottom, 15)
+                TextFieldComponentView(title: "IBeacon Minor", placeholder: "ex 12", answer: $homeVM.iBeaconVM.minor)
+                    .padding(.bottom, 15)
+            }
+            .vAlign(.top)
+            .padding(.top, 30)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        homeVM.isAddButtonClicked = false
+                    }
+                }
+                
+                ToolbarItem(placement: .principal) {
+                    Text("Add new IBeacon")
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        homeVM.addIBeacon()
+                        homeVM.isAddButtonClicked = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddIBeaconView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddIBeaconView(homeVM: HomeViewModel())
+    }
+}
