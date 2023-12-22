c24363493fb13ed6ec6605c3c85e8d933fb5a85e
handyyy <dev.handy.darmawan@gmail.com>
2023-12-22 15:36:34 +0700

    Initial Commit

---
+++ FindMyVehicle/FindMyVehicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyVehicleApp.swift
+//  FindMyVehicle
+//
+//  Created by ndyyy on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FindMyVehicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
